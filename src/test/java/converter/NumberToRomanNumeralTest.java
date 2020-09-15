bb0f14864c9d24c509d6d34be5f5a43cfb25eb21
Mads Meinert Andersen <mads485d@edu.easj.dk>
2020-09-15 12:29:16 +0200

    Tests and setup

--- src/test/java/converter/NumberToRomanNumeralTest.java
+++ src/test/java/converter/NumberToRomanNumeralTest.java
@@ -1,6 +1,7 @@
 package converter;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,15 +9,44 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NumberToRomanNumeralTest {
+    private NumberToRomanNumeralConverter test;
+
+    @BeforeEach
+    public void setup()
+    {
+        test = new NumberToRomanNumeralConverter();
+    }
 
     @Test
-    public void mustBeAbleToCreateConverter() {
+    public void testThirtyNineMustBeEqualXXXIX() {
         // Arrange
-
         // Act
-        NumberToRomanNumeralConverter test = new NumberToRomanNumeralConverter();
+        // Assert
+        assertEquals("XXXIX", test.convert(39));
+
+    }
+    @Test
+    public void testTwoHundredAndFortySixMustEqualCCXLVI() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals("CCXLVI", test.convert(246));
+
+    }
+    @Test
+    public void testSevenHundredAndEightyNineMustEqualDCCLXXXIX() {
+        // Arrange
+        // Act
         // Assert
         assertEquals("DCCLXXXIX", test.convert(789));
+
+    }
+    @Test
+    public void testTwThousandFourHundredAndTwentyOneMustEqual() {
+        // Arrange
+        // Act
+        // Assert
+        assertEquals("MMCDXXI", test.convert(2421));
 
     }
 }
